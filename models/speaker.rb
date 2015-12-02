adf8daa18bca016ba9db04caf02d8b9d0cccc533
Ben Manning <bmanning@me.com>
2015-12-01 20:34:39 -0800

    modify solution

--- models/speaker.rb
+++ models/speaker.rb
@@ -1,13 +1,11 @@
-# require "./hacktive_record.rb"
+require "./hacktive_record.rb"
 
-class Speaker
+class Speaker < HacktiveRecord::Base
 
-  attr_accessor :first_name, :last_name, :email
+  attr_accessor :first_name, :last_name, :email, :id
 
-  # def initialize(args)
-  #   @first_name = args[:first_name]
-  #   @last_name = args[:last_name]
-  #   @email = args[:email]
-  # end
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 
 end
--- models/talk.rb
+++ models/talk.rb
@@ -1,1 +1,7 @@
-# require "./hacktive_record.rb"
+require "./hacktive_record.rb"
+
+class Talk < HacktiveRecord::Base
+
+  attr_accessor :topic, :duration, :speaker_id
+
+end
